b9ffe5615705e59107f15f65029377b8bada1599
deverman-spu <deverman@spu.edu>
2018-10-28 21:45:22 -0700

    Added basic file comparison.
    Added the functionality to compare files based on their length in bytes to determine if they might be equal.

--- DupeFinder/Form1.cs
+++ DupeFinder/Form1.cs
@@ -18,6 +18,10 @@ namespace DupeFinder
             InitializeComponent();
         }
 
+        /** This function just populates the generic list with all files in the given folder
+         *  prefixing the filename with the filepath. It then returns this generic list to the
+         *  calling function.
+         **/
         private List<string> getFileList(List<string> fileList)
         {
             DirectoryInfo dir = new DirectoryInfo(txtFolderPath.Text);
@@ -25,12 +29,13 @@ namespace DupeFinder
 
             foreach (FileInfo file in dirFiles)
             {
-                fileList.Add(file.Name);
+                fileList.Add(txtFolderPath.Text + "\\" +  file.Name);
             }
 
             return fileList;
         }
 
+        /** Simple method that opens a folder browser dialog then populates our textbox with the value **/
         private void btnSelectFolder_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
@@ -39,18 +44,32 @@ namespace DupeFinder
             }
         }
 
+        /** This function will create a generic list, populate that list with all files in the specified folder,
+         *  then loop through each file comparing it to all of the other files based on file length in bytes.
+         *  
+         *  We only consider the files equal if the length in bytes is the same and the name is different. 
+         *  (We already know that the file is equal to itself)
+         **/
         private void btnCompareFiles_Click(object sender, EventArgs e)
         {
             List<string> fileList = new List<string>();
             getFileList(fileList);
 
-            string str = "";
-
             foreach (string file in fileList)
             {
-                str += file + "\n";
+                long fileSize = new System.IO.FileInfo(file).Length;
+
+                foreach (string checkFile in fileList)
+                {
+                    long checkFileSize = new System.IO.FileInfo(checkFile).Length;
+
+                    if ((fileSize == checkFileSize) && (file.Equals(checkFile) == false))
+                    {
+                        lblStatus.Text = fileSize + "  -  " + checkFileSize;
+                        MessageBox.Show(file + "\n\n" + checkFile);
+                    }
+                }
             }
-            MessageBox.Show(str);
         }
     }
 }
